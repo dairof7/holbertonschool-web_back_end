dfdb461006542d517a905a93169e1881e19b35b4
dairo <dairof7@gmail.com>
2021-10-18 19:35:32 -0500

    [add]concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+''' takes a string 1 and a string 2 as arguments
+    and returns a concatenated string
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    ''' Return concatenation of str1 and str2. '''
+    return str1 + str2
